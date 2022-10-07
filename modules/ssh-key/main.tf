023345bdf5be57d4bb7b6f4bef10aa0b5eb14e1a
Sharuq <sharuq@email.com>
2022-10-08 03:24:36 +0530

    added

--- modules/ssh-key/main.tf
+++ modules/ssh-key/main.tf
@@ -8,7 +8,7 @@ resource "tls_private_key" "key" {
 
 resource "local_file" "private_key" {
   filename          = "${var.namespace}-key.pem"
-  sensitive_content = tls_private_key.key.private_key_pem
+  local_sensitive_file = tls_private_key.key.private_key_pem
   file_permission   = "0400"
 }
 
