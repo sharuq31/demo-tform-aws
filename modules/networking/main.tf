bcc21c5f8420d6f87c388b882efd94c2be5bde8e
Sharuq <sharuq@email.com>
2022-10-08 03:21:46 +0530

    new

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -11,8 +11,8 @@ module "vpc" {
   public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24"]
   #assign_generated_ipv6_cidr_block = true
   create_database_subnet_group     = true
-  enable_nat_gateway               = true
-  single_nat_gateway               = true
+  enable_nat_gateway               = false
+  single_nat_gateway               = false
 }
 
 // SG to allow SSH connections from anywhere
